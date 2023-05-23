4714d262322b681d01948be9cb14a819b4e2df83
andikarekatias <andika.rekatias@yahoo.com>
2023-05-23 10:20:10 +0700

    update ruby and rails to latest

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
-Rails.application.routes.draw do  
-  get 'about', to: 'pages#about'
+Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "pages#home"
+  # root "articles#index"
 end
