9efd45f3a1f5c15bd4d0bc039620f6af3a4baddf
andikarekatias <andika.rekatias@yahoo.com>
2023-06-21 17:30:48 +0700

    update notice in dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,14 +1,15 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
 
+  def index    
+    @notices = Notice.all.order(created_at: :desc).limit(5)
+  end
+
   protected
 
   def unauthenticated!
     redirect_to new_user_session_path
-  end
-
-  def index
-  end
+  end  
 
   def pelabuhans
     
