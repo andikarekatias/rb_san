ac83edc26c006310ea855444809f54e1816ee130
andikarekatias <andika.rekatias@yahoo.com>
2023-06-26 15:32:51 +0700

    update master kapal

--- app/models/master_kapal.rb
+++ app/models/master_kapal.rb
@@ -11,4 +11,16 @@ class MasterKapal < ApplicationRecord
     has_one_attached :surat_ukur
     has_one_attached :surat_laut
     has_one_attached :foto_kapal
+    validate :image_file_format
+
+    private
+
+    def image_file_format
+      return unless foto_kapal.attached?
+      unless foto_kapal.content_type.in?(%w(image/jpeg image/png image/gif))
+        foto_kapal.purge
+        errors.add(:foto_kapal, 'must be a JPEG, PNG, or GIF image')
+      end
+    end
+
 end
